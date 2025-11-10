5cbf3c1a979a903899998bba6553de01dcf70690
Rahul Yadav <rahulydw.dev@gmail.com>
2025-11-10 10:56:26 +0530

    User Response Change

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import User from '../models/User.model.js';
-import generateRandomPassword from '../utils/generateRandomPassword.js';
-import generateUniqueUsername from '../utils/generateUniqueUsername.js';
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import User from "../models/User.model.js";
+import generateRandomPassword from "../utils/generateRandomPassword.js";
+import generateUniqueUsername from "../utils/generateUniqueUsername.js";
 
 // Cookie Extract:
 function cookieExtractor(req) {
@@ -14,54 +14,67 @@ function cookieExtractor(req) {
     token = req.cookies.token;
   }
   return token;
-};
+}
 
 // Passport Strategy Setup :
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_REDIRECT_URL
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ googleId: profile.id });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_REDIRECT_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
 
-    if (!user) {
-      //  Password aur Username Generate
-      let passwd = 'Chat45@'; // Or generateRandomPassword();
-      let uniqueUsername = await generateUniqueUsername(profile.displayName);
+        if (!user) {
+          //  Password aur Username Generate
+          let passwd = "Chat45@"; // Or generateRandomPassword();
+          let uniqueUsername = await generateUniqueUsername(
+            profile.displayName
+          );
 
-      //  New User Create
-      user = new User({
-        googleId: profile.id,
-        name: profile.displayName,
-        username: uniqueUsername,
-        email: profile.emails[0].value,
-        avatar: profile.photos[0].value,
-        password: passwd
-      });
+          //  New User Create
+          user = new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            username: uniqueUsername,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+            password: passwd,
+          });
 
-      await user.save();
+          await user.save();
+        }
+        const { password, createdAt, updatedAt, __v, email, ...safeUser } =
+          req.user;
+
+        return done(null, safeUser);
+      } catch (err) {
+        return done(err, null);
+      }
     }
-
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
-  }
-}));
+  )
+);
 
 // Jwt Strategy for Verify :
-passport.use(new JwtStrategy({
-  jwtFromRequest: cookieExtractor, 
-  secretOrKey: process.env.JWT_SECRET,
-}, async (payload, done) => {
-  try {
-    const user = await User.findById(payload.id);
-    if (user) return done(null, user);
-    return done(null, false);
-  } catch (err) {
-    return done(err, false);
-  }
-}));
-
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (payload, done) => {
+      try {
+        const user = await User.findById(payload.id);
+        if (user) return done(null, user);
+        return done(null, false);
+      } catch (err) {
+        return done(err, false);
+      }
+    }
+  )
+);
 
 export default passport;
